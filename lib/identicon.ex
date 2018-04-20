73765bd1b1d113c0a611e4405add12dcefb819e0
Hermes Netto <dev.hermes@hotmail.com>
2018-04-20 09:14:20 -0300

    Add some functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,14 +1,29 @@
 defmodule Identicon do
   def main(input) do
     input
-    |> hash_input
-    |> pick_color
-    |> build_grid
+      |> hash_input
+      |> pick_color
+      |> build_grid
+      |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{Image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
+    grid = 
+      hex
+        |> Enum.chunk(3)
+        |> Enum.map(&mirror_row/1)
+        |> List.flatten
+        |> Enum.with_index
+    
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
@@ -22,7 +37,7 @@ defmodule Identicon do
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+      |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
